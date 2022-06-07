e82743d5765006bc5412edd269e07e30781ab868
NasgulNexus <moctegor@gmail.com>
2022-06-07 20:47:09 +0300

    добавил json файл на 300 пользователей

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,4 +1,4 @@
-import { users } from "./data/users.js";
+import users from "./data/users.json";
 
 const handler = (req, res) => {
   if (req.method === "GET") {
--- pages/users.jsx
+++ pages/users.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import MainLayout from "../components/MainLayout.jsx";
 import UsersCard from "../components/UsersCard.jsx";
 import UsersGroup from "../components/UsersGroup.jsx";
@@ -5,12 +6,24 @@ import UsersTable from "../components/UsersTable.jsx";
 import classes from "../styles/Users.module.css";
 
 const Users = ({ users }) => {
+  const [displayType, setDisplayType] = useState("table");
+
   return (
     <MainLayout>
       <div className={classes.wrapper}>
-        <div className={classes.search}>Тут вид отображения и будет поиск</div>
+        <div className={classes.search}>
+          <select name="select">
+            <option value="table">Таблиц</option>
+            <option value="value2" selected>
+              Значение 2
+            </option>
+            <option value="value3">Значение 3</option>
+          </select>
+        </div>
         <div>
-          <UsersGroup users={users} />
+          {displayType === "table" ? <UsersTable users={users} /> : null}
+          {displayType === "card" ? <UsersCard users={users} /> : null}
+          {displayType === "group" ? <UsersGroup users={users} /> : null}
         </div>
       </div>
     </MainLayout>

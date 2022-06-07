584143e48efa39719f7777b0a6e12b60d99261f7
NasgulNexus <moctegor@gmail.com>
2022-06-07 13:31:57 +0300

    добавил индикатор загрузки

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,16 @@
+import NextNProgress from "nextjs-progressbar";
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
+      <NextNProgress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
       <Component {...pageProps} />
     </>
   );

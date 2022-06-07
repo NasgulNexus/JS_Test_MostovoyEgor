3f336e55c241c6693a87fce30a39aee25f56a802
NasgulNexus <moctegor@gmail.com>
2022-06-07 14:47:43 +0300

    добавил на первую стриницу личную информацию

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,23 @@
 import MainLayout from "../components/MainLayout.jsx";
 
 const Home = () => {
-  return <MainLayout>fist </MainLayout>;
+  return (
+    <MainLayout>
+      <div>
+        <h1>Проект сделал Мостовой Егор</h1>
+        <p>Почта: moctegor@gmail.com</p>
+        <p>Телефон: +7 (967) 157-45-92</p>
+        <p>
+          <a href="https://github.com/NasgulNexus/">github</a>
+        </p>
+        <p>
+          <a href="https://mytischi.hh.ru/resume/37591393ff080f020d0039ed1f6a5937443967">
+            резюме
+          </a>
+        </p>
+      </div>
+    </MainLayout>
+  );
 };
 
 export default Home;
